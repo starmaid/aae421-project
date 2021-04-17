5cd01a25ed1c541b22c2d875302ef712a2d124e8
mchartigan <hartigan@purdue.edu>
2021-04-17 16:14:23 -0400

    fuuck

--- Controllers/forward_mode.m
+++ Controllers/forward_mode.m
@@ -7,3 +7,11 @@ title("Forward Mode Latitude Controller Response to Step Input");
 xlabel("Time (s)");
 ylabel("Speed (m/s)");
 legend("Step Input", "Control Response", "location", "southeast");
+
+figure(2);
+plot(sine.Time, sine.Data + eq, '--'); hold on;
+plot(sine_response.Time, sine_response.Data + eq); grid on;
+title("Forward Mode Latitude Controller Response to Sine Input");
+xlabel("Time (s)");
+ylabel("Speed (m/s)");
+legend("Step Input", "Control Response", "location", "southwest");
